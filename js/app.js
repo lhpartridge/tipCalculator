040009ab5c39663520e5f0e62a479e3d34e7bd71
lhpartridge <lhpartridge@comcast.net>
2023-12-06 12:03:59 -0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,4 @@
+// Build a calculator that will take the subtotal of the tab.
+// The tip can be 15%, 10%, 20%
+// Have cards with food items, total items chosen, place in subtotal, then return subtotal, ask for tip, calcuate total, return total
+
